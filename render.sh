954eda01068980efd1539f2297bd30ca2c88c548
Ronmi Ren <ronmi.ren@gmail.com>
2016-03-01 17:05:00 +0800

    update tagname

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TAG in latest old-stable stable testing wheezy jessie stretch
+for TAG in latest oldstable stable testing wheezy jessie stretch
 do
     mkdir $TAG
     sed "s/%tag%/$TAG/g" template > $TAG/Dockerfile
