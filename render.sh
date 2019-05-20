4c6e937fb939878b294cd934164a4d753cb85877
Ronmi Ren <ronmi.ren@gmail.com>
2019-05-20 09:08:02 +0800

    update debian version

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for TAG in latest oldstable stable testing wheezy jessie stretch
+for TAG in latest oldstable stable testing jessie stretch buster
 do
     mkdir $TAG
     sed "s/%tag%/$TAG/g" template > $TAG/Dockerfile
